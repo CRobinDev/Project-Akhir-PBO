5ded687a5bb825091deede6483069e87ea5a7bd6
Christopher Robin <williamchen1506@gmail.com>
2024-06-04 03:52:22 +0700

    `refractor` : move file location

--- src/Implementasi_GUI/Class/RuteTravel.java
+++ src/Implementasi_GUI/Class/RuteTravel.java
@@ -2,13 +2,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package GUI;
+package Implementasi_GUI.Class;
 
 /**
  *
  * @author dhani
  */
 public enum RuteTravel {
+
     SURABAYA_MALANG,
     MADURA_MALANG,
     BANYUWANGI_MALANG,
